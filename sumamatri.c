cd85258bf5a39b19b3c3c49701998d73a9a7cba6
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-11-29 16:09:04 -0500

    Segunda matriz y se imprime las dos matrices

--- sumamatri.c
+++ sumamatri.c
@@ -5,12 +5,21 @@ int main()
 {
     int filas;
     int columnas;
+    int filas2;
+    int columnas2;
     printf("Ingrese Filas Primera Matriz: ");
     scanf("%d", &filas);
     printf("Ingrese Columnas Primera Matriz: ");
     scanf("%d", &columnas);
+    printf("Ingrese Filas Primera Matriz: ");
+    scanf("%d", &filas2);
+    printf("Ingrese Columnas Primera Matriz: ");
+    scanf("%d", &columnas2);
     int matriz[filas][columnas];
+    int matrizdos[filas2][columnas2];
     srand(time(NULL));
+    printf("\nPrimera matriz ");
+    printf("\n");
     /*Tener matriz aleatorias*/
     for (int i = 0; i < filas; i++)
     {
@@ -19,7 +28,7 @@ int main()
             matriz[i][j] = rand() % 100 + 1;
         }
     }
-    /*Imprimir las matriz*/
+    /*Imprimir la matriz*/
     for (int i = 0; i < filas; i++)
     {
         for (int j = 0; j < columnas; j++)
@@ -27,6 +36,29 @@ int main()
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
+    }
+    /*Tener matrizdos aleatorias*/
+    printf("\nSegunda matriz");
+    printf("\n");
+    for (int i = 0; i < filas2; i++)
+    {
+        for (int j = 0; j < columnas2; j++)
+        {
+            matrizdos[i][j] = rand() % 100 + 1;
+        }
+    }
+    /*Imprimir la matriz dos*/
+    for (int i = 0; i < filas2; i++)
+    {
+        for (int j = 0; j < columnas2; j++)
+        {
+            printf("%d ", matrizdos[i][j]);
+        }
+        printf("\n");
+    }
+    while (filas == filas2 && columnas == columnas2)
+    {
+        /* code */
     }
 
     return 0;
